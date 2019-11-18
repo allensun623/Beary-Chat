721524a753b2ec51a861b728496b34026532e05a
AllenSun7 <sunlun7@yahoo.com>
2019-11-18 11:13:19 -0800

    random user-agent

--- detect/amzn_calculator.py
+++ detect/amzn_calculator.py
@@ -4,19 +4,30 @@ import requests
 from lxml import html, etree
 import pysnooper
 import bearychat_send as bs
+import random
 
 """get the updated news from gate.io"""
 
 @pysnooper.snoop()
 def product():
     #user agent
-    url_detail = "https://www.amazon.com/dp/B01DZRY4HE/ref=twister_B00WS2T4ZA?_encoding=UTF8&th=1"  
-    user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+    url_detail = "https://www.amazon.com/dp/B01NAZGQEA/ref=twister_B00WS2T4ZA?_encoding=UTF8&th=1"  
+    user_agents =['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.8 (KHTML, like Gecko) Beamrise/17.2.0.9 Chrome/17.0.939.0 Safari/535.8',
+                'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.36 Safari/535.7',
+                'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.0 Safari/532.5',
+                'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; SV1; .NET CLR 1.1.4322)',
+                'Mozilla/5.0(Macintosh;U;IntelMacOSX10_6_8;en-us)AppleWebKit/534.50(KHTML,likeGecko)Version/5.1Safari/534.50',
+                'Mozilla/5.0(Windows;U;WindowsNT6.1;en-us)AppleWebKit/534.50(KHTML,likeGecko)Version/5.1Safari/534.50',
+                'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+                'Opera/9.80(Macintosh;IntelMacOSX10.6.8;U;en)Presto/2.8.131Version/11.11',
+                'Opera/9.80(WindowsNT6.1;U;en)Presto/2.8.131Version/11.11']  
+    #random user agent
+    i = random.randint(0,8)
+    user_agent = user_agents[i]
     HEADERS = {'User-Agent':user_agent,
                 'Referer': "www.google.com"}
- 
     count = 0
-    #Because of anti-scrapy, run until get the information
+    #Because of anti-scrapy, running until get the information or up to 50x
     while True:
         count += 1
         response = requests.get(url_detail, headers=HEADERS)
@@ -34,9 +45,6 @@ def product():
             product_title = "Failed to get infomation"
             break
 
-    #print("product_title: ", product_title[0].strip())
-    #print("product_price: ", product_price[0])
-    #print("Attemps: %d" %count)
     news_dictionary = {"product": "price"}
     if product_price[0] == "$139.98": # if price changed
         d = {product_title[0].strip(): "Price: " + product_price[0]}
