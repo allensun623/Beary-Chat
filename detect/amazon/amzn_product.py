90ad4aea423b970d68ee1a028013e8bfcad80d22
AllenSun7 <sunlun7@yahoo.com>
2019-11-28 11:22:49 -0600

    amzn html_tree

--- detect/amazon/amzn_product.py
+++ detect/amazon/amzn_product.py
@@ -13,9 +13,40 @@ from fake_useragent import UserAgent
 def __product(url_product, product_title_xpath, product_price_xpath, current_price):
     #user agent
     url_detail = url_product
-    user_agent = UserAgent().random
-    HEADERS = {'User-Agent':user_agent,
-                'Referer': "www.google.com"}
+    count = 0
+    #Because of anti-scrapy, running until get the information or up to 50x
+    while True:
+        count += 1
+        html_etree = html_request(url_detail)
+        # get the result of price and title
+        product_title = html_etree.xpath(product_title_xpath) 
+        product_price = html_etree.xpath(product_price_xpath) 
+        # break when get info or fails
+        if product_price or count > 30:
+            break
+    #store data to dictionary and then return
+    news_dictionary = {"product": "Failed to get infomation",
+                        "price":"$$$"}
+    #title
+    try:
+        d_product = {"product": product_title[0]}
+        news_dictionary.update(d_product)
+    except:
+        pass
+    #price
+    try:
+        if product_price[0] == current_price: # if price changed
+            d_price = {"price": "Price: " + product_price[0]}
+        else:
+            d_price = {"price": "Original price: " + current_price + "\n" + "Sale: " + product_price[0]}
+        news_dictionary.update(d_price)
+    except:
+        pass
+
+    return news_dictionary
+
+def html_request(url_detail):
+    #return html request with cookie, header
     cookies = 'v=3; \
                 iuuid=1A6E888B4A4B29B16FBA1299108DBE9CDCB327A9713C232B36E4DB4FF222CF03; \
                 webp=true; \
@@ -29,33 +60,12 @@ def __product(url_product, product_title_xpath, product_price_xpath, current_pri
     for line in cookies.split(';'):
         name, value = cookies.strip().split('=', 1)
         cookie[name] = value
-    count = 0
-    #Because of anti-scrapy, running until get the information or up to 50x
-    while True:
-        count += 1
-        response = requests.get(url_detail, cookies=cookie, headers=HEADERS)
-        html_etree = etree.HTML(response.content.decode('utf-8'))
-        # get the result of price and title
-        product_title = html_etree.xpath(product_title_xpath) 
-        product_price = html_etree.xpath(product_price_xpath) 
-        # break when get info or fails
-        if product_price or count > 30:
-            break
-    #store data to dictionary and then return
-    news_dictionary = {"product": "Failed to get infomation",
-                        "price":"$$$"}
-    try:
-        if product_price[0] == current_price: # if price changed
-            d_price = {"price": "Price: " + product_price[0]}
-        else:
-            d_price = {"price": "Original price: " + current_price + "\n" + "Sale: " + product_price[0]}
-        news_dictionary.update(d_price)
-        d_product = {"product": product_title[0]}
-        news_dictionary.update(d_product)
-    except:
-        pass
-
-    return news_dictionary
+    user_agent = UserAgent().random
+    HEADERS = {'User-Agent':user_agent,
+                'Referer': "www.google.com"}
+    response = requests.get(url_detail, cookies=cookie, headers=HEADERS)
+    html_etree = etree.HTML(response.content.decode('utf-8'))
+    return html_etree
 
 def detect(data):
     # return product price and name from amazon    
