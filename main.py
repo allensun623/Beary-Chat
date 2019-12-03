acd32831130b0d4397a3d23f0656fb691cbc5eca
AllenSun7 <sunlun7@yahoo.com>
2019-12-02 21:32:05 -0600

    add send_interval

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 from detect.gate import gate_news, gate_startup
 from detect.amazon import amzn
 from detect.sweaters import sweater_data
+import send_interval as sil
 
 def main():
-    amzn.product_info()
-    gate_news.detect()
-    gate_startup.detect()
+    #amzn.product_info()
+    #gate_news.detect()
+    #gate_startup.detect()
+    sil.interval_processing()
 
 if __name__ == "__main__":
     main()
